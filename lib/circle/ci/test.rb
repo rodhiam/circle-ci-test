afdb1b8d480b1ef0dcbb5f50985fd122e1a23809
rodhiam <straight.tea.lipton@gmail.com>
2017-10-29 11:46:57 +0900

    RubyGemnized

---
+++ lib/circle/ci/test.rb
@@ -0,0 +1,9 @@
+require "circle/ci/test/version"
+
+module Circle
+  module Ci
+    module Test
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/circle/ci/test/version.rb
@@ -0,0 +1,7 @@
+module Circle
+  module Ci
+    module Test
+      VERSION = "0.1.0"
+    end
+  end
+end
